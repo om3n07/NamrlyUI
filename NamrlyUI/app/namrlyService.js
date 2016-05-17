b057cb4cca5cb5e2c43618a59b74bdf520c0bb2b
Matt <mattmartell07@gmail.com>
2016-05-17 11:12:23 -0400

    fixing repo

---
+++ NamrlyUI/app/namrlyService.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+    var serviceId = 'namrlyService';
+    angular.module('rlybitsApp').factory(serviceId, ['$http', namrlyService]);
+
+    function namrlyService($http) {
+        var serviceBase = "http://namrly.azurewebsites.net/api/";
+        var getStartupName = function (includeImmatureSuffixes) {
+            var serviceAddress = serviceBase + "Namrly";
+            if (includeImmatureSuffixes && includeImmatureSuffixes === true) {
+                serviceAddress += "?includeImmatureSuffixes=true";
+            }
+
+            return $http.get(serviceAddress);
+        }
+
+        var service = {
+            GetStartupName: getStartupName
+        };
+
+        return service;
+    }
+})();
