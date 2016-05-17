44a94d060c77aa264af7e1ce9a65807fcafd3e9e
Matt <mattmartell07@gmail.com>
2016-05-17 10:22:04 -0400

    Initial Check in

---
+++ NamrlyUI/app/app.js
@@ -0,0 +1,7 @@
+﻿
+(function () {
+    'use strict';
+    var rlybitsApp = angular.module('rlybitsApp', []);
+})();
+
+
---
+++ NamrlyUI/obj/Release/Package/PackageTmp/app/app.module.js
@@ -0,0 +1,6 @@
+﻿(function () {
+    'use strict';
+
+    var rlybitsApp = angular.module('rlybitsApp', []);
+
+})();
